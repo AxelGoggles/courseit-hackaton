e9d0f168968d62b429a66394abbcbce2f8fb1ef2
Franco Tegaldi <francoteg@gmail.com>
2021-02-15 15:52:25 -0300

    fixed logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import styles from "./Logo.module.scss";
+import Link from 'next/link'
 
 const Logo = () => {
   return (
+    <Link href='/'>
     <div className={styles.container}>
       <span className={styles.adopt}>Adopt</span>
       <span className={styles.app}>ápp</span>
       <span className={`material-icons ${styles.heart}`}>favorite_border</span>
     </div>
+    </Link>
   );
 };
 
