e1639f154b18b9df4182f5b4182ae310c95a2bf3
xsamskellington <samantha.valdez.toccalino@gmail.com>
2021-02-15 02:07:52 -0600

    asd

--- components/PersonalInfo/PersonalInfo.js
+++ components/PersonalInfo/PersonalInfo.js
@@ -5,37 +5,37 @@ function PersonalInfo(){
     <>
     <section>
         <h1 id="title">♥ FORMULARIO DE ADOPTANTE ♥</h1>
-        <p id="description">Contanos un poco sobre vos!</p>
+        <p id="description">Contanos un poco sobre vos :)</p>
     </section>
 
     <form>
         <div>
-            <label for="firstName">Nombre</label>
+            <label for="firstName"><p>Nombre</p></label>
             <input type="text" name="firstName" className="firstName" required/>
         </div>
 
         <div>
-            <label for="lastName">Apellido</label>
+            <label for="lastName"><p>Apellido</p></label>
             <input type="text" name="lastName" className="lastName" required/>
         </div>
 
         <div>
-            <label for="birthday">Fecha de Nacimiento</label>
+            <label for="birthday"><p>Fecha de Nacimiento</p></label>
             <input type="date" name="birthday" className="birthday" required/>
         </div>
         
         <div>
-            <label for="address">Dirección</label>
+            <label for="address"><p>Dirección</p></label>
             <input type="text" name="address" className="address" required/> 
         </div>
         
         <div>
-            <label for="phoneNumber">Teléfono</label>
+            <label for="phoneNumber"><p>Teléfono</p></label>
             <input type="tel" name="phoneNumber" className="phoneNumber" required/>
         </div>
         
         <div>
-            <label for="email">Email</label>
+            <label for="email"><p>Email</p></label>
             <input type="email" name="email" className="email" required/>
         </div>
 
